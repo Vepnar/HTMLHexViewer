38ba67b348e3de781ef8de987adeb76527e25e35
Vepnar <agdehaan@gmail.com>
2021-11-18 20:26:09 +0100

    Refactor for PyPi

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+from typing import List
+from setuptools import setup, find_packages
+
+CURRENT_DIR = Path(__file__).parent
+
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def get_long_description() -> str:
+    return (CURRENT_DIR / "README.md").read_text(encoding="utf8")
+
+
+setup(
+    name="HTMLHexViewer",
+    version="0.1.0",
+    description="Create simple hexdumps from any file",
+    long_description=get_long_description(),
+    author="Arjan de Haan",
+    author_email="agdehaan@gmail.com",
+    url="https://github.com/Vepnar/HTMLHexViewer",
+    license="GPL-3",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.6.2",
+    install_requires=get_requirements(),
+    entry_points={
+        "console_scripts": [
+            "htmlhexviewer=htmlhexviewer:main",
+        ]
+    },
+    package_data={'htmlhexviewer': ['templates/*']},
+)
--- src/htmlhexviewer/__init__.py
+++ src/htmlhexviewer/__init__.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+
+__version__ =  "0.1.0"
+__author__ = "Arjan de Haan"
+
 import argparse
 
 from pathlib import Path
 
 from typing import Iterator, List, Tuple
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader
 
 
 class ByteData:
@@ -24,7 +29,7 @@ class ByteData:
         return padded_hex(self.value, 2)
 
 
-def byte_reader(path: str):
+def byte_reader(path: str) -> Iterator[str]:
     with open(path, "rb") as reader:
         b = reader.read(1)
         while b:
@@ -45,7 +50,7 @@ def read_all(path: str) -> str:
         return reader.read()
 
 
-def read_binary(path: str, offset=0, length=0) -> Iterator[str]:
+def read_binary(path: str, offset=0, length=0) -> List[ByteData]:
     read_bytes = []
     for index, read_byte in enumerate(byte_reader(path)):
         if offset > index:
@@ -88,7 +93,7 @@ def parse_args() -> Tuple:
         "-r", "--read", type=int, help="Amount of bytes to read", default=0
     )
     parser.add_argument(
-        "-o", "--output", type=argparse.FileType("w"), default="./report.html"
+        "-o", "--output", type=Path, default="./report.html"
     )
     parser.add_argument("source_file", type=Path)
     return parser.parse_args()
@@ -96,27 +101,31 @@ def parse_args() -> Tuple:
 
 def main():
     current_location = Path(__file__).parent
-    script_path = Path(current_location, "../templates/script.js").absolute()
-    style_path = Path(current_location, "../templates/style.css").absolute()
-    template_path = Path(current_location, "../templates").absolute()
+    script_path = Path(current_location, "templates/script.js").absolute()
+    style_path = Path(current_location, "templates/style.css").absolute()
+    template_path = Path(current_location, "templates").absolute()
 
     env = Environment(loader=FileSystemLoader(template_path))
     env.globals.update(padded_hex=padded_hex)
 
     args = parse_args()
-
     source_path = args.source_file
 
     # Load external script & styling.
     script = f"<script>{read_all(script_path)}</script>"
     style = f"<style>{read_all(style_path)}</style>"
 
+    print('Reading binary...')
     read_bytes = read_binary(source_path, offset=args.offset, length=args.read)
+    print('Rendering template...')
     output = render_template(
         env, read_bytes, title=source_path, style=style, script=script
     )
-    with args.output as writer:
+    print(f'Writing to {args.output}...')
+    with open(args.output, "w") as writer:
         writer.write(output)
+
+    print('Done.')
 
 
 if __name__ == "__main__":
